7b8cc06b357c9919162006907c11a6aad4401d43
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-10 17:39:29 +0530

    A title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(base = '')
+    suffix = 'Twitter Stats App'
+    if base.empty?
+      suffix
+    else
+      [base, suffix].join(' | ')
+    end
+  end
 end
