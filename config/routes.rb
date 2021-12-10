68e14ff1b0bb32bf16bea2b63e9aa49998177f82
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-10 23:34:54 +0530

    show user data to logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
-
+  
   get '/auth/auth0/callback', to: 'auth0#callback'
   get '/auth/failure', to: 'auth0#failure'
   get '/auth/logout', to: 'auth0#logout'
+
+  get '/dashboard', to: 'dashboard#show'
 end
