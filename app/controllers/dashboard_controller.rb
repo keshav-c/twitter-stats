3dd07d12c2f39421f936615de44a8a662322722b
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-12 10:58:58 +0530

    get followers for new user from Twitter API

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,16 @@ class DashboardController < ApplicationController
 
   def show
     # symbol keys become strings on redirect
-    @user = get_userinfo(session['data']['userid'])
+    userid = session['data']['userid']
+    @user = get_userinfo(userid)
 
-    app_user = User.find_or_create_by!(
-      handle: @user[:username],
-      twitterid: @user[:twitter_id]
-    )
+    if !User.exists? (twitterid: userid)
+      user = User.create!(
+        handle: @user[:username],
+        twitterid: @user[:twitter_id]
+      )
+      followers = get_followers(userid)
+    end
   end
 
   private 
@@ -22,12 +26,27 @@ class DashboardController < ApplicationController
     response = HTTP.auth(header).get(url, params: params)
     data = JSON.parse(response.body)['data']
     {
-      twitter_id: data['id'],
       username: data['username'],
       name: data['name'],
       tweet_count: data['public_metrics']['tweet_count'],
       followers_count: data['public_metrics']['followers_count'],
       following_count: data['public_metrics']['following_count'],
     }
+  end
+
+  def get_followers(userid)
+    url = "https://api.twitter.com/2/users/#{userid}/followers"
+    header = "Bearer #{ENV['TWITTER_STATS_BEARER']}"
+    params = { max_results: 500 }
+    followers = []
+    loop do
+      response = HTTP.auth(header).get(url, params: params)
+      data = JSON.parse(response.body)
+      followers += data['data']
+      next_token = data['meta']['next_token']
+      break if next_token.nil?
+      params[:pagination_token] = next_token
+    end
+    followers.map! { |f| { twitterid: f['id'], handle: f['username'] } }
   end
 end
